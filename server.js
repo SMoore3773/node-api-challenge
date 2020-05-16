89948f4600b41c3807e48db9fe41460c1ec8a67a
SMoore3773 <Smooresculpture@gmail.com>
2020-05-16 14:39:42 -0400

    projects and actions routers CRUD working

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const projectRouter = require('./projectRouter');
+const actionsRouter = require('./actionsRouter');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>web Node sprint challenge</h2>`);
